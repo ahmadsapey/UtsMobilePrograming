f39af7a6bb5e0cbcc46fe4841d7b35222c4f7168
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2025-10-23 19:19:57 +0700

    login

---
+++ lib/uts/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class tampilanLogin extends StatelessWidget {
+  const tampilanLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("Latihan Stak"),
+      backgroundColor: Colors.amber,
+      foregroundColor: Colors.blue,
+      ),
+
+      );
+  }
+}
